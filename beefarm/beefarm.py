c553f292ba1eebe7129b3925cfb2d48386e030aa
good_cho <cho.cho@goodoc.co.kr>
2017-04-24 19:11:17 +0900

    http get

--- beefarm/beefarm.py
+++ beefarm/beefarm.py
@@ -1,1 +1,10 @@
-print('helloworld')
+import threading
+import urllib.request
+
+def func(*args):
+    #thread process is here
+    print(urllib.request.urlopen("http://www.google.co.kr").read())
+
+thread = threading.Thread(target=func, args=(1, 2))
+thread.start()
+#thread.join()
