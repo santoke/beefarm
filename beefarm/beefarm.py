3861b299cc156d646e1ea850820ff4f90028c91f
ls <cho@amazon.com>
2017-05-12 00:21:54 +0900

    문서 파싱

--- beefarm/beefarm.py
+++ beefarm/beefarm.py
@@ -20,6 +20,8 @@ def get_inside(url):
     #doc = pq(url='http://45.77.19.179:1337/?url=http://www.goodoc.co.kr/events/5883?funnel=organic')
     try:
         doc = pq(url='http://45.77.19.179:1337/?url=http://' + valid_domain + url)
+        if url[:6] == '?v=jav':
+            parse_doc(doc)
     except:
         print("Get URL Error:", url)
         return
@@ -35,8 +37,45 @@ def get_inside(url):
     #data = urlencode({ "chksum" : "23f07fae59a1a1eb160b145521269207" }).encode()
     #print(req.urlopen(url, data).read())
 
+def list_genres(index, node):
+    d = pq(node)
+    print(d.attr('id'), d.text())
+
+def list_cast(index, node):
+    d = pq(node)
+    id = d.attr('id')
+    name = d.find('.star').text()
+    alias = d.find('.alias').text()
+    print(id, name, alias)
+
+def list_comment(index, node):
+    d = pq(node)
+    id = d.attr('id')
+    print(id)
+    #print(d('.t').html().split('[img]'))
+
+def parse_doc(doc):
+    content_area = doc('#rightcolumn')
+    print('title is:', content_area('#video_title').text())
+    jacket_info = content_area('#video_jacket_info')
+    print('video id is:', jacket_info('#video_id').text())
+    print('jacket image url:', jacket_info('#video_jacket_img').attr('src'))
+    print('release date:', jacket_info('#video_date').find('text').text())
+    print('video_length:', jacket_info('#video_length').find('.text').text())
+    print('director:', jacket_info('#video_director').find('.director').text())
+    print('maker:', jacket_info('#video_maker').find('.maker').text())
+    print('label:', jacket_info('#video_label').find('.label').text())
+    print('genres')
+    jacket_info('#video_genres').find('.genre').each(list_genres)
+    print('cast')
+    jacket_info('#video_cast').find('.cast').each(list_cast)
+    print('comments')
+    content_area('#video_comments').find('.comment').each(list_comment)
+
 def func(*args):
-    get_inside('/en/')
+    #get_inside('/en/')
+    doc = pq(url='http://45.77.19.179:1337/?url=http://www.javlibrary.com/en/?v=javlikic6e')
+    parse_doc(doc)
 
 thread = threading.Thread(target=func, args=(1, 2))
 thread.start()
