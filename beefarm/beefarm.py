afc863d18f38adeeee1ca9b6f4f6cb7e693709d6
good_cho <cho.cho@goodoc.co.kr>
2019-01-28 18:27:49 +0900

    test 와 production 모드

--- beefarm/beefarm.py
+++ beefarm/beefarm.py
@@ -4,22 +4,27 @@ from app.document import Document
 
 Config()
 
-def start():
-    if True: # 디버깅 모드로 테스트 모듈 진입
-        # 셀레늄으로 쿠키 가져오기 예제
-        #driver = webdriver.Chrome()
-        #url = Config.d['domain'] + '/' + Config.d['language'] + '/'
-        #driver.get(url)
-        #all_cookies = driver.get_cookies()
-        #print(all_cookies)
+# 디버깅 모드로 테스트
+def test():
+    # 셀레늄으로 쿠키 가져오기 예제
+    #driver = webdriver.Chrome()
+    #url = Config.d['domain'] + '/' + Config.d['language'] + '/'
+    #driver.get(url)
+    #all_cookies = driver.get_cookies()
+    #print(all_cookies)
 
-        d = Document()
-        #genres = d.get_genre_list_page_links()
-        last_link = d.get_video_last_paging_links('vl_genre.php?g=da')
-        print(last_link)
-    else:
-        get_genre_list_page_links()
-        #get_video_list_links('vl_genre.php?g=am')
+    d = Document()
+    #genres = d.get_genre_list_page_links()
+    last_link = d.get_video_last_paging_links('vl_genre.php?g=da')
+    print(last_link)
+
+def start():
+    d = Document()
+    arr_genres = d.get_genre_list_page_links()
+    for genre in arr_genres:
+        last_page = d.get_video_last_paging_links(genre)
+        d.iterate_paging(last_page)
 
 if __name__ == "__main__":
-    start()
+    test()
+    #start()
