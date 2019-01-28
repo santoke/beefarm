ef17a533c148899117612f9a015cc856d497750e
good_cho <cho.cho@goodoc.co.kr>
2019-01-28 18:27:18 +0900

    connection information

--- beefarm/database.py
+++ beefarm/database.py
@@ -2,7 +2,8 @@ import sqlalchemy
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = sqlalchemy.create_engine("mysql://root:rntekrrntekr1!@45.77.19.179:4306/farms?use_unicode=True&charset=utf8", encoding='utf-8', pool_size=100, max_overflow=200)
+connection_string = f"mysql://{config.d['database']['user']}:{config.d['database']['password']}@{config.d['database']['address']}:{config.d['database']['port']}/{config.d['database']['db']}"
+engine = sqlalchemy.create_engine(f"{connection_string}?use_unicode=True&charset=utf8", encoding='utf-8', pool_size=2, max_overflow=200)
 db_session = scoped_session(sessionmaker(bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
