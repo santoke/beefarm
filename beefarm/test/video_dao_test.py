bc8dda1695b497600df7df35b92dbae060606e82
good_cho <cho.cho@goodoc.co.kr>
2019-02-01 17:03:12 +0900

    tearUp, tearDown 테스트 데이터 생성/삭제

--- beefarm/test/video_dao_test.py
+++ beefarm/test/video_dao_test.py
@@ -2,14 +2,42 @@ from app.config import Config
 from database import Connection
 from app.video_dao import VideoDAO
 
+from models.video import Video
+
 import unittest
 
 class VideoDAOTest(unittest.TestCase):
 
     def setUp(self):
         Config()
-        Connection()
+
         self.v = VideoDAO()
+        self.test_id = 'TEST-123'
+
+        self.v.session.add(Video(self.test_id))
+        self.v.commit()
 
     def test_get_video(self):
-        self.v.get_video('NSPS-773')
+        video = self.v.get_video(self.test_id)
+        self.assertEqual(video.id, self.test_id)
+
+    def test_add_if_not_exist(self):
+        # case 1. 데이터가 이미 존재함
+        video = self.v.add_if_not_exist(self.test_id)
+        self.v.commit()
+
+        self.assertEqual(video.id, self.test_id)
+
+        # case 2. 데이터가 없어서 새로 생성해야 할 경우
+        self.v.session.delete(video)
+        self.v.commit()
+        video = self.v.add_if_not_exist(self.test_id)
+        self.v.commit()
+
+        self.assertEqual(video.id, self.test_id)
+
+    def tearDown(self):
+        test_video = Video.query.filter_by(id=self.test_id).first()
+        if test_video:
+            self.v.session.delete(test_video)
+        self.v.commit()
