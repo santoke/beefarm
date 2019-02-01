1f21c792fd94163e1685817431a9e931363e7f75
good_cho <cho.cho@goodoc.co.kr>
2019-02-01 17:02:43 +0900

    모델 데이터를 생성자 인자로 받지 않는다

--- beefarm/models/video.py
+++ beefarm/models/video.py
@@ -15,13 +15,13 @@ class Video(ModelBase):
     maker_id = Column(Integer)
     label_id = Column(Integer)
 
-    def __init__(self, id, site_url, subject, image, release_date, length):
+    def __init__(self, id):
         self.id = id
-        self.site_url = site_url
-        self.subject = subject
-        self.image = image
-        self.release_date = release_date
-        self.length = length
+        #self.site_url = site_url
+        #self.subject = subject
+        #self.image = image
+        #self.release_date = release_date
+        #self.length = length
 
     def __repr__(self):
         return "<User(id='%s', subject='%s', release_date='%s')>" % (self.id, self.subject, self.release_date)
